b4a2719b747635e35adc05680453860f9db75aba
goldin2008 <goldin20082011@gmail.com>
2020-09-28 09:17:11 -0400

    Update.

--- project4-Operationalize-ML-Microservice-API/run_kubernetes.sh
+++ project4-Operationalize-ML-Microservice-API/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="goldin2008/project-ml-microservice-kubernetes:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservices --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microservices 8000:80
 
+# Get logs
+kubectl logs `kubectl get pods -o=name`
